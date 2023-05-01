4047ae65857ec16b9e634fae464cd8302e3cc438
PrezBala <prez5aw@live.com>
2023-05-01 16:49:00 +0000

    create class meta for rating under models.py for both user and movie

--- api/models.py
+++ api/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 
 class Movie(models.Model):
@@ -10,3 +11,8 @@ class Movie(models.Model):
 class Rating(models.Model):
     movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+    stars = models.IntegerField(validators=[MinValueValidator(1),MaxValueValidator(5)])
+
+    class Meta:
+        unique_together = (('user', 'movie'),)
+        index_together = (('user', 'movie'),)
