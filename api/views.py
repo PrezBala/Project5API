70e015b5f892358393653913bab22fd0632fdb93
PrezBala <prez5aw@live.com>
2023-05-01 21:09:56 +0000

    create movieviewset under views.py and rest framework import

--- api/views.py
+++ api/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from rest_framework import viewsets, status
+from .models import Movie, Rating
 
-# Create your views here.
+
+class MovieViewSet(viewsets.ModelViewSet):
+    queryset = Movie.objects.all()
+    serializer_class = MovieSerializer
+    authentication_classes = (TokenAuthentication, )
+    permission_classes = (IsAuthenticated,)
