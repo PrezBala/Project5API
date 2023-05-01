d4d18b5bac18f8b0f1747ed7715bcdfc2ac5d048
PrezBala <prez5aw@live.com>
2023-05-01 21:11:34 +0000

    correct error within views.py and add import movie,ratinng and user serializer

--- api/views.py
+++ api/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
 from rest_framework import viewsets, status
 from .models import Movie, Rating
+from .serializers import MovieSerializer, RatingSerializer, UserSerializer
 
 
 class MovieViewSet(viewsets.ModelViewSet):
     queryset = Movie.objects.all()
     serializer_class = MovieSerializer
-    authentication_classes = (TokenAuthentication, )
-    permission_classes = (IsAuthenticated,)
+
+
+class RatingViewSet(viewsets.ModelViewSet):
+    queryset = Rating.objects.all()
+    serializer_class = RatingSerializer
