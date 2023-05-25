c5005149f7d3ba701385c08a10d3547882352ee6
PrezBala <>
2023-05-25 10:23:27 +0000

    add DIRS within templates in settings.py

--- movierater/settings.py
+++ movierater/settings.py
@@ -77,13 +77,20 @@ CORS_ALLOW_CREDENTIALS = True
 
 ROOT_URLCONF = 'movierater.urls'
 
-TEMPLATES_DIR = os.path.join(BASE_DIR, "templates")
 TEMPLATES = [
     {
-        ...
-        'DIRS': [TEMPLATES_DIR],
-        ...
-    }
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
 ]
 
 
