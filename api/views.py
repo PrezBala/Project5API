88c0d40a8e7ef5df05fc269952bc55c5bcaeec01
PrezBala <>
2023-06-08 12:44:09 +0000

    amend user permission in views.py

--- api/views.py
+++ api/views.py
@@ -2,7 +2,7 @@ from rest_framework import viewsets, status
 from rest_framework.response import Response
 from rest_framework.decorators import action
 from rest_framework.authentication import TokenAuthentication
-from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from django.contrib.auth.models import User
 from .models import Movie, Rating
 from .serializers import MovieSerializer, RatingSerializer, UserSerializer
@@ -11,19 +11,25 @@ from .serializers import MovieSerializer, RatingSerializer, UserSerializer
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
-    permission_classes = (AllowAny,)
+    permission_classes = (IsAdminUser,)
 
 
 class MovieViewSet(viewsets.ModelViewSet):
     queryset = Movie.objects.all()
     serializer_class = MovieSerializer
-    authentication_classes = (TokenAuthentication, )
+    authentication_classes = (TokenAuthentication,)
     permission_classes = (IsAuthenticated,)
+
+    def get_permissions(self):
+        if self.action == 'rate_movie':
+            permission_classes = [IsAdminUser]
+        else:
+            permission_classes = self.permission_classes
+        return [permission() for permission in permission_classes]
 
     @action(detail=True, methods=['POST'])
     def rate_movie(self, request, pk=None):
         if 'stars' in request.data:
-
             movie = Movie.objects.get(id=pk)
             stars = request.data['stars']
             user = request.user
@@ -33,12 +39,10 @@ class MovieViewSet(viewsets.ModelViewSet):
                 rating.stars = stars
                 rating.save()
                 serializer = RatingSerializer(rating, many=False)
-                response = (
-                    {
-                        'message': 'Rating updated',
-                        'result': serializer.data
-                    }
-                )
+                response = {
+                    'message': 'Rating updated',
+                    'result': serializer.data
+                }
                 return Response(response, status=status.HTTP_200_OK)
             except Rating.DoesNotExist:
                 rating = Rating.objects.create(
@@ -47,28 +51,34 @@ class MovieViewSet(viewsets.ModelViewSet):
                     stars=stars
                 )
                 serializer = RatingSerializer(rating, many=False)
-                rating = Rating.objects.create(
-                    user=user,
-                    movie=movie,
-                    stars=stars
-                )
+                response = {
+                    'message': 'Rating created',
+                    'result': serializer.data
+                }
                 return Response(response, status=status.HTTP_200_OK)
 
         else:
-            response = {'message': 'provide stars'}
+            response = {'message': 'Provide stars'}
             return Response(response, status=status.HTTP_400_BAD_REQUEST)
 
 
 class RatingViewSet(viewsets.ModelViewSet):
     queryset = Rating.objects.all()
     serializer_class = RatingSerializer
-    authentication_classes = (TokenAuthentication, )
+    authentication_classes = (TokenAuthentication,)
     permission_classes = (IsAuthenticated,)
 
+    def get_permissions(self):
+        if self.action in ['update', 'create']:
+            permission_classes = [IsAdminUser]
+        else:
+            permission_classes = self.permission_classes
+        return [permission() for permission in permission_classes]
+
     def update(self, request, *args, **kwargs):
-        response = {'message': 'You cant update rating like that'}
+        response = {'message': 'You cannot update the rating like that'}
         return Response(response, status=status.HTTP_403_FORBIDDEN)
 
     def create(self, request, *args, **kwargs):
-        response = {'message': 'You cant create rating like that'}
+        response = {'message': 'You cannot create the rating like that'}
         return Response(response, status=status.HTTP_403_FORBIDDEN)
