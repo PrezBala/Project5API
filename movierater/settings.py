2f15058a48cea98fc118f22b20daa3c0e4285ec4
PrezBala <>
2023-09-08 20:40:03 +0000

    removed api admin from settings.py to resolve error

--- movierater/settings.py
+++ movierater/settings.py
@@ -57,7 +57,6 @@ MIDDLEWARE = [
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'api.middleware.AdminMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
